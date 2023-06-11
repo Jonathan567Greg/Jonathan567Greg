239b943c056702e80d0d6ba237261d77bf089d33
Jonathan567Greg <135286410+Jonathan567Greg@users.noreply.github.com>
2023-06-10 21:04:31 -0600

    Create servicios.js
      <h1>{Login}</h1>
                            <input placeholder="Usuario"/>
                            <input placeholder="Contraseña"/>
                        </div>
                    </div>
                </div>
    
            </>import styles from "../styles/servicios.module.css"
    
    
    const servicio = ()=>{//COMPONENTE
       //JS Tradicional
       //Emac6
        console.log("Hola mundo")
    
        let Login = "Login Test"
    
       
        return(// Sintaxis JSX html tradicional etiquetas class"clasesCSS"
            <>
                <div className={styles.contenedor}>
                    <div className={styles.contenedorContenido}>
    
                        <div className={styles.contenedorElementos}>
                            
        )
    
    }
    
    export default servicio

---
+++ servicios.js
@@ -0,0 +1,30 @@
+  <h1>{Login}</h1>
+                        <input placeholder="Usuario"/>
+                        <input placeholder="Contraseña"/>
+                    </div>
+                </div>
+            </div>
+
+        </>import styles from "../styles/servicios.module.css"
+
+
+const servicio = ()=>{//COMPONENTE
+   //JS Tradicional
+   //Emac6
+    console.log("Hola mundo")
+
+    let Login = "Login Test"
+
+   
+    return(// Sintaxis JSX html tradicional etiquetas class"clasesCSS"
+        <>
+            <div className={styles.contenedor}>
+                <div className={styles.contenedorContenido}>
+
+                    <div className={styles.contenedorElementos}>
+                        
+    )
+
+}
+
+export default servicio
