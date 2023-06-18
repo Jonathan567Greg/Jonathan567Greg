4789b523224cfba24f77e86264ffef1272ffa5e9
Jonathan567Greg <135286410+Jonathan567Greg@users.noreply.github.com>
2023-06-18 15:24:58 -0600

    Update next.config.js
    @@ -0,0 +1,6 @@
    /** @type {import('next').NextConfig} */
    const nextConfig = {
      reactStrictMode: true,
    }
    
    module.exports = nextConfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-@@ -0,0 +1,7 @@
-{
-  "compilerOptions": {
-    "paths": {
-      "@/*": ["./src/*"]
-    }
-  }
+@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
- 6 changes: 6 additions & 0 deletions6  
-next.config.js
+
+module.exports = nextConfig
